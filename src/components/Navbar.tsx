6e151793574d4086db1995e32a865a897115a887
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-01-28 16:01:11 +0100

    Add ConnectWallet component (#13)
    Allows to connect a wallet using web3Modal.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
 import Logo from "./svg/Logo";
+
+const ConnectWallet = dynamic(() => import("./ConnectWallet"), {
+  ssr: false,
+});
 
 export const Header = (): JSX.Element => {
   return (
@@ -54,24 +60,7 @@ export const Header = (): JSX.Element => {
           </a>
         </nav>
         <div className="flex items-center ml-auto">
-          <button
-            className="flex h-6 ml-3 text-sm rounded-full bg-better-violet focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-            aria-haspopup="true"
-          >
-            <span className="sr-only">Open user menu</span>
-            <svg
-              className="w-6 h-6 text-better-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+          <ConnectWallet />
         </div>
       </div>
     </header>
---
+++ src/hooks/web3Modal.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: process.env.INFURA_ID,
+    },
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet",
+  cacheProvider: true,
+  providerOptions,
+});
+
+export function useWeb3Modal() {
+  const [provider, setProvider] = useState<
+    ethers.providers.Web3Provider | undefined
+  >();
+
+  async function connectWallet() {
+    const externalProvider = await web3Modal.connect();
+    const ethersProvider = new ethers.providers.Web3Provider(externalProvider);
+
+    setProvider(ethersProvider);
+  }
+
+  function disconnectWallet() {
+    web3Modal.clearCachedProvider();
+    setProvider(undefined);
+  }
+
+  return { connectWallet, disconnectWallet, provider };
+}
