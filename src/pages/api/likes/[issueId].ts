7a8ab57ae135d4cc323be031f1d4d4dec993cdcb
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-01-30 20:27:55 +0100

    Add likesHandler next api

---
+++ src/pages/api/likes/[issueId].ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import {
+  likeIssue,
+  dislikeIssue,
+  getLikes,
+  disconnect,
+} from "../../../utils/db";
+
+export default async function likesHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    query: { issueId },
+    body: { like, dislike },
+    method,
+  } = req;
+
+  switch (method) {
+    case "GET": {
+      const { likesCount } = await getLikes(issueId);
+      res.status(200).json({ issueId, likesCount });
+      break;
+    }
+    case "PUT": {
+      if (like) {
+        await likeIssue(issueId);
+      }
+
+      if (dislike) {
+        await dislikeIssue(issueId);
+      }
+
+      const { likesCount } = await getLikes(issueId);
+      res.status(200).json({ issueId, likesCount });
+      break;
+    }
+    default:
+      res.setHeader("Allow", ["GET", "PUT"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  await disconnect();
+}
