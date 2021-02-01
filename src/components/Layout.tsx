45c5b36c81636113de8a52cca20b23a7c4e95e12
isidorosp <isidoros.passadis@gmail.com>
2021-02-01 09:45:34 +0100

    feat: Add a footer
    Adds a footer to later contain links to Better resources as well as links to project resources (defined in the config file)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,16 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout(props) {
   return (
-    <div className="antialiased bg-gray-100 v-full">
+    <div className="flex flex-col antialiased bg-gray-100 v-full">
       <Header siteTitle={props.siteTitle} />
       <Navbar />
-      <div className="flex flex-wrap items-center justify-center content">
+      <div className="flex flex-wrap items-center justify-center pb-16 bg-gray-100 content">
         {props.children}
       </div>
+      <Footer />
     </div>
   );
 }
