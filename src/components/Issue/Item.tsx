8c6b545db9f745dbf7b13deae43d7ca86aa518fb
chezhe <chezhe@hey.com>
2021-11-06 00:52:20 +0800

    add contract to store likes count

--- src/components/Issue/Item.tsx
+++ src/components/Issue/Item.tsx
@@ -3,22 +3,40 @@ import { Issue } from '../../../types'
 import { formatDate } from '../../utils/text'
 import { useLikeIssue } from '../../hooks/issue'
 import IssueTags from './Tags'
+import { useEffect, useState } from 'react'
+import { getContract, getAccount } from '../../utils/near'
 
 interface Props {
   issue: Issue
 }
 
 export default function IssueItem({ issue }: Props) {
-  const { data, isValidating, likeIssue, dislikeIssue, didLike } = useLikeIssue(
-    issue.number
-  )
+  const [likes, setLikes] = useState<string[]>([])
+  const [address, setAddress] = useState<string>('')
+
+  useEffect((): void => {
+    getAccount()
+      .then((account) => {
+        setAddress(account.accountId)
+      })
+      .catch((error) => {})
+
+    getContract().then((contract: any) => {
+      contract.getIssueLikes({ issueId: `${issue.id}` }).then((likes) => {
+        console.log(likes)
+        setLikes(likes)
+      })
+    })
+  }, [])
+
+  const didLike = likes.includes(address)
 
   const handleClickLike = async () => {
-    await likeIssue()
-  }
-
-  const handleClickDislike = async () => {
-    await dislikeIssue()
+    getContract().then((contract: any) => {
+      contract.likeIssue({ issueId: `${issue.id}` }).then(() => {
+        console.log('liked')
+      })
+    })
   }
 
   return (
@@ -29,7 +47,7 @@ export default function IssueItem({ issue }: Props) {
             <span className="block text-xs font-medium uppercase text-better-black">
               Popularity
             </span>
-            {data?.likesCount}
+            {likes.length}
           </div>
 
           <div>
@@ -82,43 +100,23 @@ export default function IssueItem({ issue }: Props) {
         </div>
         <div className="flex issue__sidebar-buttons">
           <div className="w-1/2 text-center bg-better-yellow rounded-bl-xl font-poppins">
-            {didLike ? (
-              <button
-                className={`w-full h-full p-4 ${
-                  isValidating && 'cursor-not-allowed'
-                }`}
-                onClick={handleClickDislike}
-                disabled={isValidating}
+            <button
+              className={`w-full h-full p-4 ${didLike && 'cursor-not-allowed'}`}
+              onClick={handleClickLike}
+              disabled={!didLike}
+            >
+              <svg
+                className="inline w-6 h-6 mx-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  className="inline w-6 h-6 mx-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" />
-                </svg>
-                <span className="font-bold align-middle">DISLIKE</span>
-              </button>
-            ) : (
-              <button
-                className={`w-full h-full p-4 ${
-                  isValidating && 'cursor-not-allowed'
-                }`}
-                onClick={handleClickLike}
-                disabled={isValidating}
-              >
-                <svg
-                  className="inline w-6 h-6 mx-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                </svg>
-                <span className="font-bold align-middle">LIKE</span>
-              </button>
-            )}
+                <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+              </svg>
+              <span className="font-bold align-middle">
+                {didLike ? 'LIKED' : 'LIKE'}
+              </span>
+            </button>
           </div>
           <div className="w-1/2 text-center bg-better-green rounded-br-xl font-poppins">
             <button className="w-full h-full p-4">
--- src/utils/near.ts
+++ src/utils/near.ts
@@ -1,31 +1,42 @@
-import { connect, keyStores, WalletConnection } from 'near-api-js'
+import { connect, keyStores, WalletConnection, Contract } from 'near-api-js'
 import { getConfig } from './config'
 
 const getNearWallet = async () => {
-  const nearConfig = getConfig('mainnet')
+  const nearConfig = getConfig('testnet')
   const near = await connect({
     ...nearConfig,
     keyStore: new keyStores.BrowserLocalStorageKeyStore(),
   })
 
   const wallet = new WalletConnection(near, '')
-  return wallet
+  return { wallet, near }
 }
 
 export const getAccount = async () => {
-  const wallet = await getNearWallet()
+  const { wallet } = await getNearWallet()
   const account = wallet.account()
 
   return account
 }
 
 export const connectWallet = async () => {
-  const wallet = await getNearWallet()
+  const { wallet } = await getNearWallet()
   wallet.requestSignIn(
-    'better.near', // contract requesting access
+    'chezhe.testnet', // contract requesting access
     'Better' // optional
     // "http://YOUR-URL.com/success", // optional
     // "http://YOUR-URL.com/failure" // optional
   )
   return wallet.account()
+}
+
+export const getContract = async () => {
+  const { wallet, near } = await getNearWallet()
+  const account = wallet.account()
+  const contract = new Contract(account, 'chezhe.testnet', {
+    viewMethods: ['getIssueLikes'],
+    changeMethods: ['likeIssue'],
+  })
+
+  return contract
 }
