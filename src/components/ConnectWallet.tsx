ebf78563d0778761c16dece13f0c0a02b4fe5d8e
chezhe <chezhe17@gmail.com>
2021-11-05 11:33:32 +0800

    add wallet connect for near

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -1,39 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Blockies from "react-blockies";
+import * as nearAPI from "near-api-js";
+import { getConfig } from "../utils/config";
 
-import { useWeb3Modal } from "../hooks/web3Modal";
+async function initNear() {
+  const nearConfig = getConfig("testnet");
+  // Initializing connection to the NEAR node.
+  const near = await nearAPI.connect({
+    ...nearConfig,
+    keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
+  });
 
-const truncateAddress = (address: string): string => {
-  return address.slice(0, 6) + "..." + address.slice(-4);
-};
+  console.log(near);
+
+  // Initializing Wallet based Account. It can work with NEAR TestNet wallet that
+  // is hosted at https://wallet.nearprotocol.com
+  const wallet = new nearAPI.WalletConnection(near);
+  const account = wallet.account();
+  if (account.accountId) {
+    return account;
+  } else {
+    wallet.requestSignIn(
+      "example-contract.testnet", // contract requesting access
+      "Better" // optional
+      // "http://YOUR-URL.com/success", // optional
+      // "http://YOUR-URL.com/failure" // optional
+    );
+    return wallet.account();
+  }
+}
 
 export default function ConnectWallet() {
   const [signerAddress, setSignerAddress] = useState<string>("");
-  const { connectWallet, disconnectWallet, provider } = useWeb3Modal();
-
-  useEffect(() => {
-    if (provider) {
-      const signer = provider.getSigner();
-      signer.getAddress().then((address) => setSignerAddress(address));
-    } else {
-      setSignerAddress("");
-    }
-  }, [provider]);
-
-  const handleClickConnect = async () => {
-    await connectWallet();
-  };
-
-  const handleClickAddress = () => {
-    disconnectWallet();
-  };
 
   return (
     <button
       className="flex items-center py-0.5 text-sm font-medium text-gray-600 bg-better-purple rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
       aria-haspopup="true"
-      onClick={signerAddress ? handleClickAddress : handleClickConnect}
-      title={signerAddress ? "Click to Disconnect" : "Click to Connect"}
+      onClick={async () => {
+        try {
+          const account = await initNear();
+          setSignerAddress(account.accountId);
+        } catch (error) {
+          //
+        }
+      }}
     >
       {signerAddress && (
         <div className="rounded-full border-better-white mx-1 my-0.5">
@@ -50,7 +61,7 @@ export default function ConnectWallet() {
           !signerAddress && "ml-3"
         } align-middle text-better-violet`}
       >
-        {signerAddress ? truncateAddress(signerAddress) : "Connect Wallet"}
+        {signerAddress ? signerAddress : "Connect Wallet"}
       </div>
     </button>
   );
